5a6fd051c41bb7585c23a33f052714ee9a249f0e
Basilio <basilio.alvarez@gmail.com>
2019-02-17 23:53:24 +0100

    Observables en carga de imagenes

--- src/app/load-ml5/load-ml5.component.ts
+++ src/app/load-ml5/load-ml5.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Location } from '@angular/common';
+// import { from, observable } from 'rxjs';
+import * as Rx from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
+
 import * as ml5 from 'ml5';
 import * as p5 from 'p5';
 import 'p5/lib/addons/p5.sound';
 import 'p5/lib/addons/p5.dom';
+import { from, of } from 'rxjs';
 
 interface ITrainDataImage {
   type: string;
@@ -27,11 +32,11 @@ export class LoadMl5Component implements OnInit {
 
   test1() {
     const image = document.getElementById('image');
-    const classifier = ml5.imageClassifier('MobileNet', function() {
+    const classifier = ml5.imageClassifier('MobileNet', function () {
       console.log('Model Loaded!');
     });
 
-    classifier.predict(image, function(err, results) {
+    classifier.predict(image, function (err, results) {
       console.log('Predicho');
       console.log(results[0].className);
       console.log(results[0].probability.toFixed(4));
@@ -77,6 +82,78 @@ export class LoadMl5Component implements OnInit {
     );
   }
 
+  testObservable() {
+    this.testObserver().subscribe(
+      (val: string) => console.log(val),
+      (err) => console.error(err),
+      () => console.log('completado observable 1')
+    );
+    /*this.loadImage('../../assets/imgSrc/typeA/WIN_20190130_16_34_27_Pro.jpg').subscribe(
+      (val) => {
+        console.log('cargada imagen');
+      },
+      (err) => {
+        console.error(err);
+      });*/
+    this.http.get('../../assets/imgSrc/imgList.json', {}).pipe(
+      mergeMap((data: any) => this.imgsSrcs$(data)),
+      mergeMap((data: any) => this.loadImage$(data))
+    ).subscribe(
+      (value: any) => {
+        console.log(value);
+      },
+      (err) => console.error(err),
+      () => console.log('acabado algo')
+    );
+
+    /* this.http.get('../../assets/imgSrc/imgList.json', {}).subscribe(
+      (data: any) => {
+        console.log('ok');
+        this.imgsSrcs$(data).subscribe(
+          (item) => {
+            console.log(item);
+          },
+          (err) => console.error(err),
+          () => {
+            console.log('finalizado entrenamiento');
+          }
+
+        );
+      },
+      (error) => {
+        console.error(error);
+      }
+    );*/
+
+  }
+
+  testObservable2(){
+    const d1 = [1, 2, 3, 4, 5];
+    const o$ = from(d1);
+
+    const o2$ = o$.pipe(
+      mergeMap( (val: any) => of(this.ch1(val))),
+      mergeMap( (val: any) => of(this.ch2(val))),
+    );
+    let c = 0;
+    o2$.subscribe(
+      (val) => {
+        console.log(`${c} - '${val}'`);
+        c++;
+      },
+      (err) => console.error(err),
+      () => console.log('Acabose')
+    );
+  }
+
+  ch1(val: number): string{
+    return `${val}A`;
+  }
+
+  ch2(val: string): string{
+    return `${val}B`;
+  }
+
   protected train(data: Array<ITrainDataImage>) {
     if (!this.featureExtractor) {
       this.featureExtractor = ml5.featureExtractor('MobileNet');
@@ -84,7 +161,6 @@ export class LoadMl5Component implements OnInit {
     if (!this.classifier) {
       this.classifier = this.featureExtractor.classification();
     }
-
     for (const group of data) {
       const label = group.type;
       const baseDir = Location.joinWithSlash('../../assets/imgSrc', group.dir);
@@ -94,7 +170,6 @@ export class LoadMl5Component implements OnInit {
         const img = new Image();
         img.src = dir;
         img.onload = () => {
-
           this.classifier.addImage(img, label);
           console.log(`imagen cargada como ${label}`);
         };
@@ -102,5 +177,61 @@ export class LoadMl5Component implements OnInit {
       }
     }
   }
+
+  /**
+   * Carga la imagen
+   * @param {string} imagePath
+   * @returns {Rx.Observable<HTMLImageElement>}
+   * @memberof LoadMl5Component
+   */
+  loadImage$(imagePath: {url: string}): Rx.Observable<any> {
+    return Rx.Observable.create(function(observer: Rx.Subscriber<any>) {
+      const res: {url: string, img?: any} = Object.assign({}, imagePath);
+      const img = new Image();
+      img.src = imagePath.url;
+      img.onload = function() {
+        res.img = img;
+        observer.next(res);
+        observer.complete();
+      };
+      img.onerror = function(err) {
+        observer.error(err);
+      };
+    });
+  }
+
+  testObserver(): Rx.Observable<string> {
+    return Rx.Observable.create(function (observer: Rx.Subscriber<string>) {
+      observer.next('Hello');
+      observer.next('World');
+      observer.complete();
+    });
+  }
+
+  /** Devuelve un par  url y label a asignar a esa imagen
+   * @param {Array<ITrainDataImage>} data
+   * @returns {Rx.Observable<any>}
+   * @memberof LoadMl5Component
+   */
+  imgsSrcs$(data: Array<ITrainDataImage>): Rx.Observable<any> {
+    return Rx.Observable.create(function (observer: Rx.Subscriber<any>) {
+      for (const group of data) {
+        const label = group.type;
+        const baseDir = Location.joinWithSlash('../../assets/imgSrc', group.dir);
+        for (const fileName of group.files) {
+          const dir = Location.joinWithSlash(baseDir, fileName);
+          observer.next({ url: dir, label });
+          // this.classifier.addImage(mg, label);
+        }
+      }
+      observer.complete();
+    });
+  }
+
+
+
+
+
+
 
 }
